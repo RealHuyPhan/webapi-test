9873397f17a5ad6391be0ac3ff0ab352d10bec0b
DESKTOP-RL3RG5V\Huy Phan <huyps@oneempower.com>
2024-07-18 16:29:58 +0700

    update HTTP CRUD

---
+++ MyWebApiApp/MyWebApiApp/Models/HangHoa.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHoangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
